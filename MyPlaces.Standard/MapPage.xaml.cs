ed0a869c677955e95cb14560372cb45950670a0f
fugu2 <sacha.schwab@gmail.com>
2017-12-01 11:44:24 +0100

    MapPage ready to receive categoryId and list places of either all categories (dummy) or specific category (if coming from list of places of selected Category)

--- MyPlaces.Standard/MapPage.xaml.cs
+++ MyPlaces.Standard/MapPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MyPlaces.Standard.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -9,9 +10,10 @@ namespace MyPlaces.Standard
 {
     public partial class MapPage : ContentPage
     {
-        ViewModels.MapViewModel viewModel;
         Data.DataAccessLayer dataAccessLayer = new Data.DataAccessLayer();
         Data.MapCenter mapCenter = new Data.MapCenter();
+        ViewModels.MapViewModel mapViewModel;
+        Data.Category currentCategory;
 
         public MapPage()
         {
@@ -20,14 +22,45 @@ namespace MyPlaces.Standard
             if (Device.RuntimePlatform == Device.iOS)
                 Padding = new Thickness(0, 20, 0, 0);
 
-            BindingContext = viewModel = new ViewModels.MapViewModel();
+            // Constructor with dummy category = 0
+            var category = new Data.Category
+            {
+                CategoryId = 0,
+                Name = "Some name",
+                Color = "Red"
+            };
 
+            mapViewModel = new MapViewModel(category);
+            currentCategory = category;
+            Console.WriteLine("Category ID = {0}", currentCategory.CategoryId);
+            BindingContext = mapViewModel;
+        }
+
+        public MapPage(MapViewModel mapViewModel)
+        {
+            InitializeComponent();
+            BindingContext = this.mapViewModel = mapViewModel;
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            var places = await dataAccessLayer.GetAllPhotos();
+
+            List<Data.Place> places;
+
+            /* We come from the category list page if the current category is dummy.
+             * Otherwise, we come from a specific category places view.
+             * In the dummy case, generate list of places of ALL categories.
+             * Otherwise, list places of the specific category.
+             */
+            if (currentCategory.CategoryId != 0)
+            {
+                places = await dataAccessLayer.GetAllPhotosByCategoryId(currentCategory.CategoryId);
+            }
+            else
+            {
+                places = await dataAccessLayer.GetAllPhotos();
+            }
 
             PresentMap(places);
         }
@@ -46,6 +79,12 @@ namespace MyPlaces.Standard
             // With extreme points coordinates, set the span / radius of the map
             var distance = Data.DistanceCalculator.GetDistanceInMetres(extrCoords.BiggestLat, extrCoords.BiggestLong,
                                                                        extrCoords.SmallestLat, extrCoords.SmallestLong);
+
+            // Define minimum distance so not to zoom in 'too much' if e.g. there is only one pin
+            if (distance <= 1000)
+            {
+                distance = 1000;
+            }
             // With center coordinates, set the center map point
             Karte.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(centerCoords.CentreLat, centerCoords.CentreLong),
                 Distance.FromMeters(distance)));
